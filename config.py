a0e71a3fc28dd60b88a3532ba6f302d9b6d40c97
SimplyFaisal <d.f.animashaun@gmail.com>
2015-01-20 15:53:09 -0500

    added schools

---
+++ config.py
@@ -0,0 +1,18 @@
+GOOGLE_API_KEY = 'AIzaSyCVJ2UcVsLeWUFFCtMng25Mqw_INdRwyzc'
+YIKYAK_USER_ID = 'A19A029A-EE2A-4D89-AD7F-85B1BC3459EE'
+
+SCHOOLS =  [
+    "Georgia Tech",
+    'University of Georgia',
+    'College of Charleston',
+    'University of Virginia',
+    'Penn State University',
+    'University of Alabama',
+    'James Madison University',
+    'Virginia Polytechnic Institute and State University',
+    'Georgia Southern University',
+    'University of Texas Austin',
+    'Louisiana State University',
+    'University of South Carolina',
+    'Wake Forest University',
+]
